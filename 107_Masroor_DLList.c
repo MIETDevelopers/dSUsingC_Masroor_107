a9d771e8f6e362d7fd5cfdbc1dc73b206638b5b8
masroor07 <76279044+masroor07@users.noreply.github.com>
2021-08-27 10:06:46 +0530

    Double Liked List

---
+++ 107_Masroor_DLList.c
@@ -0,0 +1,147 @@
+#include<stdio.h>  
+#include<stdlib.h>  
+struct node  
+{  
+    struct node *prev;  
+    struct node *next;  
+    int data;  
+};  
+struct node *head;  
+void insertion_beginning();  
+void deletion_beginning();  
+void display();  
+void search();  
+void main ()  
+{  
+int choice =0;  
+    while(choice != 5)  
+    {  
+        printf("\n*Main Menu*\n");  
+        printf("\nChoose one option from the following list ...\n");  
+        printf("\n1.Insert in begining\n2.Delete from Beginning\n3.Search\n4.Display\n5.Exit\n");  
+        printf("\nEnter your choice?\n");  
+        scanf("\n%d",&choice);  
+        switch(choice)  
+        {  
+            case 1:  
+            insertion_beginning();  
+            break; 
+            case 2:  
+            deletion_beginning();  
+            break; 
+            case 3:  
+            search();  
+            break;  
+            case 4:  
+            display();  
+            break;  
+            case 5:
+            exit(0);  
+            break;  
+            default:  
+            printf("Please enter valid choice..");  
+        }  
+    }  
+}  
+void insertion_beginning()  
+{  
+   struct node *ptr;   
+   int item;  
+   ptr = (struct node *)malloc(sizeof(struct node));  
+   if(ptr == NULL)  
+   {  
+       printf("\no space");  
+   }  
+   else  
+   {  
+    printf("\nEnter Item value");  
+    scanf("%d",&item);  
+      
+   if(head==NULL)  
+   {  
+       ptr->next = NULL;  
+       ptr->prev=NULL;  
+       ptr->data=item;  
+       head=ptr;  
+   }  
+   else   
+   {  
+       ptr->data=item;  
+       ptr->prev=NULL;  
+       ptr->next = head;  
+       head->prev=ptr;  
+       head=ptr;  
+   }  
+   printf("\nNode inserted\n");  
+}  
+     
+}  
+void deletion_beginning()  
+{  
+    struct node *ptr;  
+    if(head == NULL)  
+    {  
+        printf("\n UNDERFLOW");  
+    }  
+    else if(head->next == NULL)  
+    {  
+        head = NULL;   
+        free(head);  
+        printf("\nnode deleted\n");  
+    }  
+    else  
+    {  
+        ptr = head;  
+        head = head -> next;  
+        head -> prev = NULL;  
+        free(ptr);  
+        printf("\nnode deleted\n");  
+    }  
+  
+}  
+void display()  
+{  
+    struct node *ptr;  
+    printf("\n printing values...\n");  
+    ptr = head;  
+    while(ptr != NULL)  
+    {  
+        printf("%d\n",ptr->data);  
+        ptr=ptr->next;  
+    }  
+}   
+void search()  
+{  
+    struct node *ptr;  
+    int item,i=0,flag;  
+    ptr = head;   
+    if(ptr == NULL)  
+    {  
+        printf("\nEmpty List\n");  
+    }  
+    else  
+    {   
+        printf("\nEnter item which you want to search?\n");   
+        scanf("%d",&item);  
+        while (ptr!=NULL)  
+        {  
+            if(ptr->data == item)  
+            {  
+                printf("\nitem found at location %d ",i+1);  
+                flag=0;  
+                break;  
+            }   
+            else  
+            {  
+                flag=1;  
+            }  
+            i++;  
+            ptr = ptr -> next;  
+        }  
+        if(flag==1)  
+        {  
+            printf("\nItem not found\n");  
+        }  
+    }     
+          
+}
